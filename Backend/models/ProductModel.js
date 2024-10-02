b59f6436619c20e09b4f9c6c67d2be1cbce93a2c
Hussien320 <hussienzoughaib@gmail.com>
2024-10-02 19:26:56 +0300

    git commit "a";

--- Backend/models/ProductModel.js
+++ Backend/models/ProductModel.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
-
+import AutoIncrementFactory from 'mongoose-sequence';
+import dotenv from 'dotenv';
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+const connection = mongoose.createConnection(MONGO_URI);
+const AutoIncrement = AutoIncrementFactory(connection);
 
 const ProductSchema = mongoose.Schema({
+    Id: {
+        type: Number,
+        unique: true
+    },
     Name: {
         type: String,
         required: true
@@ -23,6 +32,8 @@ const ProductSchema = mongoose.Schema({
         default: 0,
     },
 }, { timestamps: true });
+
+ProductSchema.plugin(AutoIncrement, { inc_field: 'Id' });
 
 export const Product = mongoose.model('Product', ProductSchema); // Changed 'Schema' to 'model'
 
